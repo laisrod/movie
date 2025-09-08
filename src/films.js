a2ff9be50cce76ac19f86dbc7e39c7ca8e851726
Laís Rodrigues <devlaisrodrigues@gmail.com>
2025-09-08 12:21:18 -0300

    Impleent function getMoviesFromDirector

--- src/films.js
+++ src/films.js
@@ -5,10 +5,11 @@ function getAllDirectors(array) {
   return result;
 }
 
-
 // Exercise 2: Get the films of a certain director
 function getMoviesFromDirector(array, director) {
-  
+  let result = array.filter(movie => movie.director === director);
+  console.log("EXERCICE 2 ->", result);
+  return result;
 }
 
 // Exercise 3: Calculate the average of the films of a given director.
